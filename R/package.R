e7f2a0dd29481e14ab48e5cb748837b4319c05fe
John Coene <jcoenep@gmail.com>
2018-03-07 23:16:45 +0100

    pkg documented

---
+++ R/package.R
@@ -0,0 +1,9 @@
+#' mjml: A package for building and sending responsive emails.
+#'
+#' The package provides an API to all \href{mjml}{https://mjml.io} tags.
+#'
+#' @section Includes templates as RStudio addins.
+#'
+#' @docType package
+#' @name mjml
+NULL
