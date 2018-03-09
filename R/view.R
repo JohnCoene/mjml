b581013d10c0025b52a546caa222b0363d8abb90
John Coene <jcoenep@gmail.com>
2018-03-09 19:15:43 +0100

    viewer FUN added

---
+++ R/view.R
@@ -0,0 +1,36 @@
+#' Viewer
+#'
+#' Preview email in the RStudio viewer
+#'
+#' @param mjml An \code{mjml} object as returned by \code{\link{mj_ml}.}
+#'
+#' @examples
+#' if(interactive()){
+#' mj_ml(
+#'   mj_body(
+#'     mj_column(
+#'       width = "20%",
+#'       mj_text("First columns")
+#'     ),
+#'     mj_column(
+#'       width = "80%",
+#'       mj_text("The body")
+#'     )
+#'   )
+#' ) %>%
+#'   mj_view()
+#' }
+#'
+#' @export
+mj_view <- function(mjml){
+  if(missing(mjml))
+    stop("missing mjml")
+
+  temp_mjml <- tempfile(fileext = ".mjml")
+  out <- mj_save_mjml(mjml, temp_mjml)
+  temp_html <- tempfile(fileext = ".html")
+  output <- mj_convert_html(out, temp_html)
+  unlink("temp_mjml", recursive = TRUE)
+  viewer <- getOption("viewer")
+  rstudioapi::viewer(output)
+}
