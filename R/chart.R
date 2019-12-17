182b57dce99ee1ea25d4536bc4b474e3cfb23ca4
John Coene <jcoenep@gmail.com>
2019-12-17 16:12:23 +0100

    correct mj_chart cht argument

--- R/chart.R
+++ R/chart.R
@@ -21,7 +21,6 @@
 #' @seealso \href{official documentation}{https://github.com/image-charts/mjml-chart}
 #'
 #' @export
-mj_chart <- function(cht = c("bvg", "p"), ...){
-  cht <- match.arg(cht)
+mj_chart <- function(cht = "bvg", ...){
   htmltools::tag("mj-chart", list(cht = cht, ...))
 }
